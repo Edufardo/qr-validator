6b5718005f2cff469af053276cc68e5fbae5b964
edufardo <eduardo.larraz.labordeta@gmail.com>
2022-04-09 22:10:16 +0200

    WIP - Adding form to create contact

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,9 +1,47 @@
-
+import { useState } from "react";
+import './home.css'
 const Home = () => {
 
+    const [contact, setContact] = useState({
+        name: '',
+        lastName: '',
+        email: '',
+        cellPhone: null
+    })
+    const sendContact = () => {
+        // e.preventDefault() 
+        console.log('Hello!')
+    }
+
     return (
-        <div>
-            Hola desde home
+        <div className="row">
+            <form className="col-12" onSubmit={() => sendContact()}>
+                <div className="col-12">
+                    <label>
+                        Nombre
+                    </label>
+                    <input type="text" value={contact.name}/> 
+                </div>
+                <div className="col-12">
+                    <label>
+                        Apellido
+                    </label>
+                    <input type="text" /> 
+                </div>
+                <div className="col-12">
+                    <label>
+                        Teléfono
+                    </label>
+                    <input type="number" /> 
+                </div>
+                <div className="col-12">
+                    <label>
+                        Correo
+                    </label>
+                    <input type="email" /> 
+                </div>
+                <button type="submit">Generar Qr!</button>
+            </form>
         </div>
     )
 }
