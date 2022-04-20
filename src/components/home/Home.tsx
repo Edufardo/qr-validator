1a57ec393f4f016acae577e19f05d8f56530d84a
edufardo <eduardo.larraz.labordeta@gmail.com>
2022-04-20 20:03:15 +0200

    WIP - Sending data to back

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,43 +2,60 @@ import { useState } from "react";
 import './home.css'
 const Home = () => {
 
-    const [contact, setContact] = useState({
+    const url = 'http://localhost:4200/api/card/create'
+    let [contact, setContact] = useState({
         name: '',
         lastName: '',
         email: '',
         cellPhone: null
     })
-    const sendContact = () => {
-        // e.preventDefault() 
-        console.log('Hello!')
+    const handleSubmit = (event: any) => {
+        event.preventDefault() 
+        fetch(url, {
+            method: 'post',
+            body: JSON.stringify({
+                name: "Eduardo"
+            })
+        }).then(response => response.json).then(data => setContact({
+            ...contact
+        }))
+
+        console.log(contact)
+    }
+
+    const setInfo = (event: any) => {
+        setContact({
+            ...contact,
+            [event.target.name]: event.target.value
+        })
     }
 
     return (
         <div className="row">
-            <form className="col-12" onSubmit={() => sendContact()}>
+            <form className="col-12" onSubmit={handleSubmit}>
                 <div className="col-12">
                     <label>
                         Nombre
                     </label>
-                    <input type="text" value={contact.name}/> 
+                    <input type="text" name="name" onChange={setInfo}/> 
                 </div>
                 <div className="col-12">
                     <label>
                         Apellido
                     </label>
-                    <input type="text" /> 
+                    <input type="text" name="lastName" onChange={setInfo}/> 
                 </div>
                 <div className="col-12">
                     <label>
                         Teléfono
                     </label>
-                    <input type="number" /> 
+                    <input type="number" name="cellPhone" onChange={setInfo}/> 
                 </div>
                 <div className="col-12">
                     <label>
                         Correo
                     </label>
-                    <input type="email" /> 
+                    <input type="email" name="email" onChange={setInfo}/> 
                 </div>
                 <button type="submit">Generar Qr!</button>
             </form>
